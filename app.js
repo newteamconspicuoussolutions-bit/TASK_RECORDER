c35a8c94150a7fc35d7912a2090d6b0646c55445
shiksha <shiksha@Conspicuouss-MacBook-Air.local>
2026-02-25 10:24:33 +0530

    deployment changes

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ webpush.setVapidDetails(
   process.env.VAPID_PRIVATE_KEY,
 );
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.json());
