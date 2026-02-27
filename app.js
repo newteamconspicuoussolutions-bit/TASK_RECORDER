6a0057ce1d4462b14b767c707e6311d0586f5d78
shiksha <shiksha@Conspicuouss-MacBook-Air.local>
2026-02-27 10:20:46 +0530

    deployment changes

--- app.js
+++ app.js
@@ -56,8 +56,9 @@ app.use(
     }),
     cookie: {
       maxAge: 24 * 60 * 60 * 1000,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+      // secure: process.env.NODE_ENV === 'production',
+      // sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+      secure: false,
     },
   }),
 );
